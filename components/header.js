6d5863ed36753bb50595a5845b61ebc5293c2f0a
Kjetil Kyrkjerud Hårtveit <kjetil.kyrkjerud.hartveit@politiet.no>
2025-04-02 17:08:20 +0200

    fix(team): side /team og header web component

---
+++ components/header.js
@@ -0,0 +1,97 @@
+class Header extends HTMLElement {
+  constructor() {
+    // Always call super first in constructor
+    super();
+  }
+
+  connectedCallback() {
+    const shadow = this.attachShadow({ mode: "open" });
+    shadow.innerHTML = /* html */ `
+        <style>
+            header {
+                background: #00263e;
+                padding: 1rem 1.5rem 1rem 1.25rem;
+                color: white;
+                position: relative;
+                container: header / inline-size;
+            }
+
+            nav {
+                display: flex;
+                align-items: center;
+                gap: 2.5rem;
+            }
+
+            nav a, 
+            .retninger-logo span {
+                border-bottom: 3px solid transparent;
+                position: relative;
+                top: 3px;
+
+                &:hover {
+                    border-color: #FDDA25;
+                }
+            }
+
+            a {
+                color: white;
+                    text-decoration: none;
+            }
+
+            .retninger-logo {
+                display: flex;
+                align-items: center; 
+                gap: .5rem;
+                grid-column: 1 / 1;
+            }
+            
+            @media (min-width: 1200px) {
+                .politi-logo {
+                    position: absolute;
+                    left: calc(50% - (115px / 2));
+                }
+            }
+            
+            @media (min-width: 800px) {
+                header {
+                    display: flex;
+                    flex-wrap: wrap;
+                    justify-content: space-between;
+                    align-items: center;
+                }
+            }
+            
+            @media (max-width: 800px) {
+                header {
+                    display: grid;
+                    grid-template-columns: 1fr 1fr;
+                    row-gap: 1.25rem;
+                }
+
+                .politi-logo {
+                    grid-column: 2 / 2;
+                    margin-left: auto;
+                }
+
+                nav {
+                    grid-column: 1 / span 2;
+                    justify-content: space-between;
+                }
+            }
+        </style>
+        <header>
+            <a class="retninger-logo" href="/">
+                <img src="/images/logo.svg" alt="" style="height: 1.75rem; width: auto;" />
+                <span style="font-size: 1.2rem;">Retninger</span>
+            </a>
+            <img class="politi-logo" src="/images/politiet.svg" alt="Politiets logo" />
+            <nav>
+                <a href="/brukeropplevelse">Brukeropplevelse</a>
+                <a href="/team">Team</a>
+                <a href="/teknologi">Teknologi</a>
+                <a href="/folk">Folk</a>
+            </nav>
+        </header>`;
+  }
+}
+customElements.define("tr-header", Header);
